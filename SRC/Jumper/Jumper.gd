84dd2d2cad33d407efdaa5b544261054c3138324
zlRyaNN <ryanyuri64@gmail.com>
2020-02-26 17:52:46 -0300

    Changed some features of player and jumper

--- SRC/Jumper/Jumper.gd
+++ SRC/Jumper/Jumper.gd
@@ -6,4 +6,5 @@ export (Vector2) var knockback_direction: = Vector2.UP
 func _on_Trigger_body_entered(body: PhysicsBody2D) -> void:	
 	if body.has_method("_apply_knockback"):
 		body._apply_knockback(knockback_direction, knockback_intensity)
+		$Anim.play("Bounce")
 		print("Entrou")
